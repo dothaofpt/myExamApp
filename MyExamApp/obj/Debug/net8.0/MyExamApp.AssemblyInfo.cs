7b877eac1cb66ad371ca2a7e075d56ac9d686e96
dothaofpt <143236377+dothaofpt@users.noreply.github.com>
2024-11-12 11:04:33 +0700

    up

--- MyExamApp/obj/Debug/net8.0/MyExamApp.AssemblyInfo.cs
+++ MyExamApp/obj/Debug/net8.0/MyExamApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyExamApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dbd6484cb1fa6a1b0b81834a8fded26db36054a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3cb448fa4cdd75bf1657a626e4fd7ba5fc373ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyExamApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyExamApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
